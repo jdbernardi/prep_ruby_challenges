3473c66e4bdd3c3dc87e9f9c5681b92b6523ad87
Joe Bernardi <joebernardi@hotmail.com>
2016-03-18 15:14:01 -0500

    finish overlap

---
+++ rectangle_overlap.rb
@@ -0,0 +1,50 @@
+=begin
+
+6. Rectangle Overlap
+
+Write a method overlap which takes two rectangles defined by the coordinates of their corners, e.g. [[0,0],[3,3]] and [[1,1],[4,6]], and determines whether they overlap. You can assume all coordinates are positive integers.
+
+> overlap( [ [0,0],[3,3] ], [ [1,1],[4,5] ] )
+=> true
+> overlap( [ [0,0],[1,4] ], [ [1,1],[3,2] ] )
+=> false
+
+=end
+
+#PROGRAM overlap(rectangle_1, rectangle_2)
+
+#compare the x and y coordinates of the arrays
+#taking the max and min of each array
+#if the min values of x
+
+def overlap(rect1, rect2)
+
+  x_min = [rect1[0][0], rect2[0][0]].max
+  puts "#{[rect1[0][0], rect2[0][0]]}"
+  puts "x_min: #{x_min}"
+  x_max = [rect1[1][0], rect2[1][0]].min
+  puts "#{[rect1[1][0], rect2[1][0]]}"
+  puts "x_max: #{x_max}"
+
+  y_min = [rect1[0][1], rect2[0][1]].max
+  puts "#{[rect1[0][1], rect2[0][1]]}"
+  puts "y_min: #{y_min}"
+  y_max = [rect1[1][1], rect2[1][1]].min
+  puts "#{[rect1[1][1], rect2[1][1]]}"
+  puts "y_max: #{y_max}"
+
+  if ((x_max <= x_min) || (y_max <= y_min))
+  return nil
+  else return [[x_min, y_min], [x_max, y_max]]
+  end
+end
+
+result = overlap([[0,1],[2,2]], [[2,1],[3,3]])
+
+if result != nil
+  puts "overlap"
+  print result
+  puts ""
+else puts "not overlapping"
+end
+
