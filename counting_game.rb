4b0044e6ca0e726a8c66bc9f4ead6f4d1d9ddf82
Joe Bernardi <joebernardi@hotmail.com>
2016-03-18 19:37:50 -0500

    add function for checking element index

--- counting_game.rb
+++ counting_game.rb
@@ -21,6 +21,8 @@ Make your method take two inputs -- the number of players and the number you're 
 #PROGRAM counting_game(number_of_players, number_counting_to)
 def counting_game(number_of_players, number_counting_to)
 
+#need to have a condition where if at last item in arr
+
 #a count will be established = 1
 count = 1
 person_count = 0
@@ -38,24 +40,36 @@ while i <= number_of_players
   i += 1
 end
 
+puts persons.last
+
 #each array will have the PERSON and the COUNT
 #the COUNT will change, the PERSON will not
 
 
 #until the number_counting_to is reached
 while count <= number_counting_to
-  puts "count: #{count}"
-  persons[person_count,1] << [person_count,count]
-  count += 1 
-  person_count += 1
-  print persons
-
 #start with the first person who COUNTs 1
 
-#put a line saying PERSON 1 says COUNT
+#check if at the last element in the array
+#if persons.index == count
+
+last_element_check(persons)
+
+    persons[person_count][1] = count
+  #put a line saying PERSON 1 says COUNT
+  puts "Person:#{person_count+1} Count:#{count}  "
+
 #check to see if COUNT is divisible by 7 
+
 #if COUNT is divisible by 7
+if count % 7 == 0
+  divisible_by_7(person_count, count, number_counting_to)
+elsif count % 11 == 0
+  divisible_by_11(person_count, count, number_counting_to)
+end
+
   #the direction in the array will reverse
+
   #the next PERSON will increase the COUNT
 #elsif the COUNT is divisble by 11
   #skip the PERSON in the array
@@ -65,9 +79,41 @@ while count <= number_counting_to
   #the PERSON in DIRECTION increments
 #end
 
+  person_count +=1 
+  count += 1
 end #end of while count <= number_counting_to
+
+
+#puts "Person #{person_count} end at the count number : #{count-1}"
 end
 
+#=====================================================
+#called when count % 7 == 0 // REVERSE DIRECTIONS
+def divisible_by_7(person, count, number_counting_to)
+  puts "7 called"
+
+end
+
+#=====================================================
+#called when count % 11 == 0 // SKIP PERSON
+def divisible_by_11(person, count, number_counting_to)
+  puts "11 caled"
+
+end
+
+#=====================================================
+#called to check if at the last element
+def last_element_check(array)
+  array.each.with_index do |x, index|
+    if index == 0
+      next
+    elsif index < array.size - 1
+      next
+    else
+      puts "last element"
+    end
+  end
+end
 
 puts "enter the number of players"
 number_of_players = gets.chomp.to_i
