55e6354e87f539b150a790e8fe220c292959af44
Joe Bernardi <joebernardi@hotmail.com>
2016-03-21 20:49:31 -0500

    revise uniques with include method

--- uniques.rb
+++ uniques.rb
@@ -6,63 +6,21 @@ uniques([1,5,”frog”, 2,1,3,”frog”])
 =end
 
 #PROGRAM uniques
-#receive an array from the user
-#check the length of the array
-#take the array, remove the first value and put it in a hash
-#then take the next value in the array and put it in the hash only 
-#if there is no value already
-
-
-
-
 def uniques(array)
-
-  i = 0
+  #create an array variable
   single_array = Array.new
-
-
-
-    
-
-  end  
-
-  puts "single array #{single_array}"
-
-
+  #go through each item in the array
+  array.each { |x| 
+    #if the array has the item, then don't add it
+    single_array << x unless single_array.include?(x)}
+    print "single: #{single_array}"
 
 end
 
+#grab an array from the user and split it up
+puts "Please enter an array separated by spaces"
+array = gets.chomp.split(" ")
+uniques(array)
 
-#while the count is less than the array length
-
-=begin
-def uniques(array)
-
-index_1 = 0 
-index_2 = 1
-
-singletons_array = Array.new
-
-while index_1 < array.length
-
-#while starting with the first element up to the last element
-  while index_2 < array.length
-  #check if the first item equals the second item
-    if array[index_1] == array[index_2]
-      #if its equal, pop the item
-      array.pop[index_1]
-    end
-  #else go to the next index
-  index_2 += 1
-end
-  #increment the 1 count
-  index_1 += 1
-
-end
-
-print array
-
-end
-=end
-uniques([1,5,"frog", 2, 1, 3, "frog"])
+#uniques([1,5,"frog", 2, 1, 3, "frog"])
 
